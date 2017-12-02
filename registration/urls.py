81f2f7727a6f883a288b3f2a3b03115027a4ae87
Abhishek Keshri <akeshari87@gmail.com>
2017-12-03 01:44:56 +0530

    login signin

---
+++ registration/urls.py
@@ -0,0 +1,30 @@
+"""registration URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url, include
+from django.contrib import admin
+from main import views as main_views
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', main_views.home, name='home'),
+    url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
+    url(r'^logout/$', auth_views.logout, {'next_page': 'signup'}, name='logout'),
+    url(r'^signup/$', main_views.signup, name='signup'),
+    url(r'^account_activation_sent/$', main_views.account_activation_sent, name='account_activation_sent'),
+    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        main_views.activate, name='activate'),
+]
